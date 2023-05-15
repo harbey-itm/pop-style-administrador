5b9f06385399e5f190bebc45539c56a8d65c17bc
Harbey Brieceno <harbeybriceno329728@correo.itm.edu.co>
2023-05-15 16:32:21 -0500

    Inicio Microservicio Administrador del Sistema

---
+++ src/main/java/co/com/popstyle/administrador/PopstyleMicroservicioAdministradorApplication.java
@@ -0,0 +1,13 @@
+package co.com.popstyle.administrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopstyleMicroservicioAdministradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopstyleMicroservicioAdministradorApplication.class, args);
+	}
+
+}
